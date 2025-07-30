cc52553435b6923d583815933f9263faa0a80c23
Nishant Chauhan <mailxnishant@gmail.com>
2025-07-30 23:24:29 +0530

    restrict auth func & improvise acme logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-20 w-20 rotate-[15deg]" />
-      <p className="text-[32px] px-2 md:text-[44px]">Acme</p>
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="px-2 text-[44px]">Acme</p>
     </div>
   );
 }
